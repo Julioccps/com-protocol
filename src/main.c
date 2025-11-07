f83fb8dc86151586a6eaa21a73687a048c3ad668
Julioccps <julio191096@gmail.com>
2025-11-07 19:17:48 -0300

    Updated src/main.c

--- src/main.c
+++ src/main.c
@@ -25,10 +25,17 @@ typedef enum{
 typedef struct {
     parser_state_t state;
     protocol_frame_t current_frame;
-    uint8_t bytes_receives;
+    uint8_t bytes_received;
     uint8_t payload_index;
 }protocol_parser_t;
 
+uint8_t calculate_checksum(const uint8_t* data, size_t length){
+    uint8_t sum = 0;
+    for(size_t i = 0; i < length; i++) {
+        sum ^= data[i];
+    }
+    return sum;
+}
 
 protocol_frame_t mount_frame(const uint8_t* message, uint8_t id, uint8_t msg_length){
     protocol_frame_t frame;
@@ -48,7 +55,7 @@ protocol_frame_t mount_frame(const uint8_t* message, uint8_t id, uint8_t msg_len
     
     memcpy(&checksum_data[2], frame.payload, msg_length);
     
-    frame.checksum = calculate_check_sum(checksum_data, 2 + msg_length);
+    frame.checksum = calculate_checksum(checksum_data, 2 + msg_length);
 
     return frame;
 }
@@ -98,16 +105,16 @@ int verify_checksum(const protocol_frame_t* frame){
     checksum_data[0] = frame->message_id;
     checksum_data[1] = frame->length;
     memcpy(&checksum_data[2], frame->payload, frame->length);
-    uint8_t calculated_checksum = calculate_check_sum(checksum_data, 2 + frame->length);
+    uint8_t calculated_checksum = calculate_checksum(checksum_data, 2 + frame->length);
     return (calculated_checksum == frame->checksum);
 }
 
 void simulate_corruption(protocol_frame_t* frame, int corruption_chance){
     if (corruption_chance > 0 && (rand() % 100 < corruption_chance)){
         int byte_to_corrupt = 2 + (rand() % (frame->length + 1)); 
-        uint8_t frame_data = (uint8_t*) frame;
+        uint8_t* frame_data = (uint8_t*)frame;
         frame_data[byte_to_corrupt] = 0x0F;
-        printf("Simulated corruption at byte ")
+        printf("Simulated corruption at byte ");
     }
 }
 
@@ -123,8 +130,50 @@ void print_frame(const protocol_frame_t* frame){
 
 void init_parser(protocol_parser_t* parser){
     parser->state = STATE_WAIT_START;
-    parser->bytes_receives = 0;
+    parser->bytes_received = 0;
     parser->payload_index = 0;
     memset(&parser->current_frame, 0, sizeof(protocol_frame_t));
 }
 
+int parse_byte(protocol_parser_t* parser, uint8_t byte){
+    switch (parser->state) {
+        case STATE_WAIT_START:
+            if (byte == START_BYTE){
+                parser->current_frame.start_byte = byte;
+                parser->state = STATE_READ_HEADER;
+                parser->bytes_received = 1;
+            }
+            break;
+
+        case STATE_READ_HEADER:
+            if (parser->bytes_received== 1){
+                parser->current_frame.message_id = byte;
+                parser->bytes_received++;
+            }
+            else if (parser->bytes_received == 2){
+                parser->current_frame.length = byte;
+                parser->payload_index = 0;
+                parser->state = STATE_READ_PAYLOAD;
+                parser->bytes_received++;
+            }
+            break;
+        case STATE_READ_PAYLOAD:
+            parser->current_frame.payload[parser->payload_index++];
+            if (parser->payload_index >= parser->current_frame.length){
+                parser->state = STATE_VERIFY_CHECKSUM;
+            }
+        case STATE_VERIFY_CHECKSUM:
+            parser->current_frame.checksum = byte;
+            if (verify_checksum(&parser->current_frame)){
+                parser->state = STATE_WAIT_START;
+                return 1; // Frame complete and valid
+            }
+            else{
+                parser->state = STATE_WAIT_START;
+                return -1; // Frame corrupted
+            }
+            break;
+    }
+    return 0; // Frame incomplete
+
+}
